d780b08abbf83cf21b0d2bdf01b4c355ad2e2132
k9i <ultrasuperhigh@gmail.com>
2018-07-15 22:51:14 +0900

    update

--- custom.el
+++ custom.el
@@ -5,85 +5,25 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
- '(compilation-message-face (quote default))
- '(cua-global-mark-cursor-color "#2aa198")
- '(cua-normal-cursor-color "#657b83")
- '(cua-overwrite-cursor-color "#b58900")
- '(cua-read-only-cursor-color "#859900")
- '(custom-enabled-themes (quote (molokai)))
- '(custom-safe-themes
-   (quote
-    ("fa28e5f3d9e9dcc6cf7e61994df91a46337e77555b5abd88d330ab93aa67b150" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(fci-rule-color "#eee8d5")
- '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
- '(highlight-symbol-colors
-   (--map
-    (solarized-color-blend it "#fdf6e3" 0.25)
-    (quote
-     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
- '(highlight-symbol-foreground-color "#586e75")
- '(highlight-tail-colors
-   (quote
-    (("#eee8d5" . 0)
-     ("#B4C342" . 20)
-     ("#69CABF" . 30)
-     ("#69B7F0" . 50)
-     ("#DEB542" . 60)
-     ("#F2804F" . 70)
-     ("#F771AC" . 85)
-     ("#eee8d5" . 100))))
- '(hl-bg-colors
-   (quote
-    ("#DEB542" "#F2804F" "#FF6E64" "#F771AC" "#9EA0E5" "#69B7F0" "#69CABF" "#B4C342")))
- '(hl-fg-colors
-   (quote
-    ("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3")))
- '(hl-paren-colors (quote ("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900")))
- '(magit-diff-use-overlays nil)
- '(nrepl-message-colors
-   (quote
-    ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
- '(pos-tip-background-color "#eee8d5")
- '(pos-tip-foreground-color "#586e75")
- '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#eee8d5" 0.2))
- '(term-default-bg-color "#fdf6e3")
- '(term-default-fg-color "#657b83")
- '(vc-annotate-background nil)
- '(vc-annotate-background-mode nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#dc322f")
-     (40 . "#c3c373730000")
-     (60 . "#b9b97d7d0000")
-     (80 . "#b58900")
-     (100 . "#a24c87870000")
-     (120 . "#9b9b87870000")
-     (140 . "#94e987870000")
-     (160 . "#8e3887870000")
-     (180 . "#859900")
-     (200 . "#5a5a94e92d2c")
-     (220 . "#43c39b9b43c3")
-     (240 . "#2d2da24c5a59")
-     (260 . "#1696a8fd70f0")
-     (280 . "#2aa198")
-     (300 . "#00009f9fa7a7")
-     (320 . "#00009797b7b7")
-     (340 . "#00008f8fc7c7")
-     (360 . "#268bd2"))))
- '(vc-annotate-very-old-color nil)
- '(weechat-color-list
-   (quote
-    (unspecified "#fdf6e3" "#eee8d5" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#657b83" "#839496")))
- '(xterm-color-names
-   ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#073642"])
- '(xterm-color-names-bright
-   ["#fdf6e3" "#cb4b16" "#93a1a1" "#839496" "#657b83" "#6c71c4" "#586e75" "#002b36"]))
+ )
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(diff-file-header ((t (:background "grey 30" :weight bold))))
+ '(diff-header ((t (:background "grey45"))))
+ '(diff-refine-added ((t (:inherit diff-refine-changed :background "#229922"))))
+ '(diff-refine-changed ((t (:background "#999922"))))
+ '(ediff-even-diff-A ((t (:background "grey 45"))))
+ '(ediff-even-diff-Ancestor ((t (:background "grey 30"))))
+ '(ediff-even-diff-B ((t (:background "grey 45"))))
+ '(ediff-even-diff-C ((t (:background "grey 45"))))
+ '(ediff-fine-diff-A ((t (:background "#992222"))))
+ '(ediff-fine-diff-B ((t (:background "#229922"))))
+ '(ediff-fine-diff-C ((t (:background "#999922"))))
+ '(ediff-odd-diff-A ((t (:background "grey 45"))))
+ '(ediff-odd-diff-Ancestor ((t (:background "grey 20"))))
+ '(ediff-odd-diff-B ((t (:background "grey 30"))))
+ '(ediff-odd-diff-C ((t (:background "grey 35")))))
