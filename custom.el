d2d48b3bea9def9fd487253ca6e6ebaed078b09a
k9i <ultrasuperhigh@gmail.com>
2018-07-15 20:34:38 +0900

    update

--- custom.el
+++ custom.el
@@ -5,9 +5,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
+ '(package-selected-packages
    (quote
-    ("a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "fa28e5f3d9e9dcc6cf7e61994df91a46337e77555b5abd88d330ab93aa67b150" default))))
+    (solarized-theme yaml-mode spacemacs-theme reverse-theme magit hc-zenburn-theme color-theme-solarized atom-one-dark-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
