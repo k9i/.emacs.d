29e1d429a4f9c20a7b98d2bf4e95f35aa60aef98
k9i <k9i>
2018-10-21 18:24:41 +0900

    moved package and user-package under init-loader

--- init.el
+++ init.el
@@ -38,40 +38,6 @@
  ((<= emacs-major-version 21) ; Emacs21 or before
     (progn (load "term/keyswap" t))))
 
-
-;;----------------------------------------------------------------------
-;; package.el
-
-;; usage:
-;; M-x package-list-package
-;; i to select package to install.
-;; x to install selected packages.
-;;
-;; no internet connection: M-x package-list-packages-no-feteh
-
-(cond ((<= emacs-major-version 23)
-       (add-to-list 'load-path (locate-user-emacs-file "lisp/package"))
-       (when (require 'package nil t)
-	 (package-initialize)))
-      (t
-       ;; Added by Package.el.  This must come before configurations of
-       ;; installed packages.  Don't delete this line.  If you don't want it,
-       ;; just comment it out by adding a semicolon to the start of the line.
-       ;; You may delete these explanatory comments.
-       (package-initialize)))
-
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-;;(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-
-;;----------------------------------------------------------------------
-;; use-package.el
-(eval-when-compile
-  ;; Following line is not needed if use-package.el is in ~/.emacs.d
-  (add-to-list 'load-path (locate-user-emacs-file "elpa"))
-  (require 'use-package))
-
 ;;----------------------------------------------------------------------
 ;; autoload-if-found from: http://www.sodan.org/~knagano/emacs/dotemacs.html
 ;; * init-loader内のファイルはエラーでも読み込み停止しないので、必須ではない。
---
+++ inits/11-use-package.el
@@ -0,0 +1,8 @@
+
+;;----------------------------------------------------------------------
+;; use-package.el
+(eval-when-compile
+  ;; Following line is not needed if use-package.el is in ~/.emacs.d
+  (add-to-list 'load-path (locate-user-emacs-file "elpa"))
+  (require 'use-package))
+
