c763edddce73fad83d5cf89306ec1fc7b471d9f5
k9i <stone.st15i@gmail.com>
2019-02-20 12:20:32 +0900

    add docker.el, move buffer-flp to use-package

---
+++ inits/50-docker.el
@@ -0,0 +1,5 @@
+;; -*- Emacs-Lisp -*-
+
+(use-package docker
+  :ensure t
+    :bind ("C-c d" . docker))
--- package-install.el
+++ package-install.el
@@ -5,7 +5,6 @@
     ;; C-u 0 M-: (変数名?)
 
     browse-kill-ring
-    buffer-flip
     dockerfile-mode
     docker-tramp
     magit
@@ -17,6 +16,14 @@
     )
   "packages to be installed")
 
+;;------------------------
+;; use-package.el でinstall (inits/50-*.el)
+;;    buffer-flip
+;;    docker
+;;    eww
+;;    smartrep
+
+;;------------------------
 ;;(require 'package)
 ;;(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 ;;(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
