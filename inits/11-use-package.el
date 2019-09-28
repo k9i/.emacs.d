bd5adfadf3f2d2ce86b5320b2846b6b1386e3f8b
k9i <k9i@k9i.org>
2019-09-28 14:08:52 +0900

    add url

--- inits/11-use-package.el
+++ inits/11-use-package.el
@@ -1,6 +1,8 @@
 
 ;;----------------------------------------------------------------------
 ;; use-package.el
+;; https://github.com/jwiegley/use-package
+
 (eval-when-compile
   ;; Following line is not needed if use-package.el is in ~/.emacs.d
   (add-to-list 'load-path (locate-user-emacs-file "elpa"))
