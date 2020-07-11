05e135cf65e80592a9eab3594d62abecea9cf4e7
k9i <k9i@k9i.org>
2020-07-11 18:24:23 +0900

    Add tempbuf

---
+++ inits/50-tempbuf.el
@@ -0,0 +1,13 @@
+;; -*- Emacs-Lisp -*-
+
+(use-package tempbuf
+;;  :ensure t
+  :hook (
+	 (custom-mode . turn-on-tempbuf-mode)
+	 (dired-mode . turn-on-tempbuf-mode)
+	 (w3-mode . turn-on-tempbuf-mode)
+	 (Man-mode . turn-on-tempbuf-mode)
+	 (view-mode . turn-on-tempbuf-mode)
+	 (magit-mode . turn-on-tempbuf-mode)
+	 )
+  )
